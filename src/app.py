03dc0aa8bc78be5bfb400934fdb678a7bea2a744
caminhathiago <caminha.thiago@gmail.com>
2025-06-01 02:52:05 +0800

    readding host

--- src/app.py
+++ src/app.py
@@ -13,6 +13,5 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0' ,debug=True)
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0' ,debug=True)
